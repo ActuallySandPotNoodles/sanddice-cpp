e4055981707344508a778f6cf68ab438c6c64a62
SandPotNoodles <77805958+ActuallySandPotNoodles@users.noreply.github.com>
2026-06-06 10:17:28 +0100

    Update download link for sanddice.desktop file

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ echo "Starting Install Of 'sanddice'..."
 echo "Downloading Stuff..."
 mkdir -p /usr/share/sandpotnoodles
 wget https://raw.githubusercontent.com/ActuallySandPotNoodles/sanddice-cpp/refs/heads/main/sanddice-new.png -O /usr/share/sandpotnoodles/sanddice.png
-wget https://raw.githubusercontent.com/ActuallySandPotNoodles/sanddice-cpp/refs/heads/main/sanddice.desktop -O /usr/share/applications/sanddice.desktop
+wget https://raw.githubusercontent.com/ActuallySandPotNoodles/sanddice-cpp/refs/heads/main/sanddice-new.desktop -O /usr/share/applications/sanddice.desktop
 if [[ "$(uname -m)" == "x86_64" ]]; then
 wget https://github.com/ActuallySandPotNoodles/sanddice-cpp/releases/download/1.0.0/sanddice_x86_64 -O /usr/bin/sanddice
 fi
